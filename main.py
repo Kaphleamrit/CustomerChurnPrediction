3dc84dc1ab787b27db3aa24a7d289be2bda79265
Amrit Kafle <akafle1@toromail.csudh.edu>
2024-11-01 11:34:04 -0700

    adds requirements.txt

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import numpy as np
 import os
 from openai import OpenAI
 import utils as ut
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 client = OpenAI(
     base_url="https://api.groq.com/openai/v1",
-    api_key=os.environ.get("GROQ_API_KEY")
+    api_key=os.getenv("GROQ_API_KEY")
 )
 
 def explain_prediction(probability, input_dict, surname):
